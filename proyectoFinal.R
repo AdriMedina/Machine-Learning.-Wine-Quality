1f00083f5b6309c5a825d717306c8a069caf47c2
Francisco José Fajardo Toril <frannnlol@gmail.com>
2015-06-21 10:55:41 +0200

    Seleccion de variables mediante CV.

--- proyectoFinal.R
+++ proyectoFinal.R
@@ -78,6 +78,76 @@ White.test <- BalanceWineWhite[-train_index, ]
 ### Selección de variables ###
 ##############################
 
+# Funcion que realiza predicciones usando el mejor subconjunto de variables calculado.
+predict.regsubsets <- function(object, newdata, id, ...)
+{
+  form <- as.formula(object$call[[2]])
+  mat <- model.matrix(form, newdata)
+  coeficientes <- coef(object, id=id)
+  xvars <- names(coeficientes)
+  mat[,xvars]%*%coeficientes
+}
+
+# Usando validación cruzada con k-iteraciones, aplicamos la técnica de "Selección del mejor subconjunto" para cada uno de los
+# k-conjuntos de entrenamiento.
+k <- 10
+set.seed(1)
+
+# Definimos los conjuntos de validación
+Red.folds <- sample(1:k, nrow(BalanceWineRed), replace=TRUE)
+White.folds <- sample(1:k, nrow(BalanceWineWhite), replace=TRUE)
+
+# Definimos la matriz que contendrá los datos.
+Red.cv.errors <- matrix(NA, k, 11, dimnames=list(NULL, paste(1:11)))
+White.cv.errors <- matrix(NA, k, 11, dimnames=list(NULL, paste(1:11)))
+
+# En cada iteracion J, los elementos de 'folds == j' corresponden al conjunto de test.
+# y el resto de elementos al conjunto de entrenamiento.
+for(j in 1:k){
+  # Obtenemos el mejor subconjunto de variables para cada conjunto de entrenamiento.
+  Red.best_fit <- regsubsets(quality~.,data=BalanceWineRed[Red.folds!=j,], nvmax=11)
+  White.best_fit <- regsubsets(quality~.,data=BalanceWineWhite[White.folds!=j,], nvmax=11)
+  
+  # Para cada uno de los tamaños de problema posibles
+  for(i in 1:11){
+    # Realizamos predicciones sobre la muestra de test usando las mejores variables escogidas
+    # en la iteracion J.
+    Red.pred <- predict(Red.best_fit, BalanceWineRed[Red.folds==j,], id=i)
+    White.pred <- predict(White.best_fit, BalanceWineWhite[White.folds==j,], id=i)
+    
+    # Calculamos el MSE para cada uno de los tamaños (1:11) y los almacenamos en la matriz
+    # cv.errors en la fila correspondiente a la iteración J-esima.
+    Red.cv.errors[j,i] <- mean( (BalanceWineRed$quality[Red.folds==j]-Red.pred)^2 )
+    White.cv.errors[j,i] <- mean( (BalanceWineWhite$quality[White.folds==j]-White.pred)^2 )
+  }
+}
+
+# Hacemos la media por columnas de todos los datos de la matriz para obtener la media
+# del error en todas la iteraciones de la validación cruzada.
+Red.mean.cv.errors <- apply(Red.cv.errors, 2, mean)
+White.mean.cv.errors <- apply(White.cv.errors, 2, mean)
+
+# Pintamos el error en función del número de variables obtenido por validación cruzada.
+par(mfrow=c(1,2))
+plot(Red.mean.cv.errors, type='b')
+plot(White.mean.cv.errors, type='b')
+
+# Escogemos el tamaño de modelo que ha obtenido el menor error cuadrático medio MSE.
+Red.best_size <- which.min(Red.mean.cv.errors)
+White.best_size <- which.min(White.mean.cv.errors)
+
+# Obtenemos los coeficientes de ese tamaño de modelo usando esta vez la muestra completa
+# y no solo la de training.
+Red.best_variables <- regsubsets(quality~., data=BalanceWineRed, nvmax=11)
+White.best_variables <- regsubsets(quality~., data=BalanceWineWhite, nvmax=11)
+
+# Y extraemos los coeficientes que obtuvieron menor MSE en la muestra de training
+Red.best_variables.coeficientes <- coef(Red.best_variables, id=Red.best_size)
+White.best_variables.coeficientes <- coef(White.best_variables, id=White.best_size)
+
+
+
+############################################################################################
 # Obtenemos el mejor conjunto de variables predictoras con "quality" como variable respuesta
 # usando para ello la muestra de training.
 Red.best_variables <- regsubsets(quality~., data=Red.training, nvmax=11)
@@ -106,10 +176,18 @@ for(i in 1:11){
   
 }
 
-# Escoger el que ha obtenido el menor error cuadrático medio MSE.
-print(Red.MSE)
-print(White.MSE)
+# Escogemos el tamaño de modelo que ha obtenido el menor error cuadrático medio MSE.
+Red.tam_modelo.menor_MSE <- which.min(Red.MSE)
+White.tam_modelo.menor_MSE <- which.min(White.MSE)
 
+# Obtenemos los coeficientes de ese tamaño de modelo usando esta vez la muestra completa
+# y no solo la de training.
+Red.best_variables <- regsubsets(quality~., data=BalanceWineRed, nvmax=11)
+White.best_variables <- regsubsets(quality~., data=BalanceWineWhite, nvmax=11)
+
+# Y extraemos los coeficientes que obtuvieron menor MSE en la muestra de training
+Red.best_variables.coeficientes <- coef(Red.best_variables, id=Red.tam_modelo.menor_MSE)
+White.best_variables.coeficientes <- coef(White.best_variables, id=White.tam_modelo.menor_MSE)
 
 
 
